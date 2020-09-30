727ef7b41d1358efc8f1c9eb9e9e76af91d61ab0
Vlad <vladislav.lacko@gmail.com>
2020-09-30 22:15:14 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// const obj = {
+//     name: "John",
+//     age: 25, 
+//     isMarried: true
+
+// };
+
+// console.log(obj.isMarried);
+
+// let arr = ['apple.jpg', 'grapes.png', 5, 'strawberry.gif', [], {}];  
+// console.log(arr[5]);
+
+// console.log(5 ** 2);
+
+// alert("Hi mate");
+
+// const result = confirm("Are you here ?");
+// console.log(result);
+
+// const answr = prompt ("Are you 18 ?", "");
+// console.log(typeof(answr));
+
+// const answers = [];
+
+// // answers[0] = prompt ('How are you?', '');
+// // answers[1] = prompt ('How old are you?', '');
+// // answers[2] = prompt ('What is your name?', ''); 
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/4`);
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+console.log(++incr);
+console.log(--decr); 
+
+const isCheked = true,
+      isClose = true;
+
+console.log(isCheked && isClose);
+
+      
