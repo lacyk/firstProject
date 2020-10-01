6e8e29b39fa2f3f90f1b23a057ba30423dea9b68
Vlad <vladislav.lacko@gmail.com>
2020-10-01 15:02:36 +0200

    personal films DB

--- js/script.js
+++ js/script.js
@@ -28,27 +28,6 @@
 
 // console.log(`https://someurl.com/${category}/4`);
 
-// const numberOfFilms = +prompt('How many films did you watch?', '');
-
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// };
-
-// const a = prompt("One of latest watched film", ''),
-//       b = prompt("How do you like it ? (0-10)", ''),
-//       c = prompt("One of latest watched film", ''),
-//       d = prompt("How do you like it ? (0-10)", '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-
-
 // if (4 == 9) {
 //     console.log('ok');
 // } else {
@@ -64,21 +43,63 @@
 //     console.log('Yes !');
 // }
 
-const num = 100;
+// let num = 50;
 
 // (num === 50) ? console.log("Ok!") : console.log('Error!');
 
-switch (num) {
-    case 49:
-        console.log('Not true');
-        break; 
-    case 100:
-        console.log('Not true');
-        break; 
-    case 50:
-        console.log('TRUE');
-        break;
-    default:
-        console.log('Not this time');
-        break;
-}
+// switch (num) {
+//     case 49:
+//         console.log('Not true');
+//         break; 
+//     case 100:
+//         console.log('Not true');
+//         break; 
+//     case 50:
+//         console.log('TRUE');
+//         break;
+//     default:
+//         console.log('Not this time');
+//         break;
+// }
+
+// while (num <= 55){
+//     console.log(num);
+//     num++;
+// } 
+
+// do {
+//     console.log(num);
+//     num++;
+// } while (num <= 55); 
+
+// for (let i = 1; i < 10 ; i++){
+//     if (i === 6) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+const numberOfFilms = +prompt('How many films did you watch?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("One of latest watched film", ''),
+      b = prompt("How do you like it ? (0-10)", ''),
+      c = prompt("One of latest watched film", ''),
+      d = prompt("How do you like it ? (0-10)", '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+console.log(personalMovieDB.count);
+console.log(a.length);
+
+
