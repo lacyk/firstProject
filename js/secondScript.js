f35b7f020d2da6c7dac896623d9a90340ed6742e
Vlad <vladislav.lacko@gmail.com>
2020-10-08 23:10:50 +0200

    object destructurisation

--- js/secondScript.js
+++ js/secondScript.js
@@ -1,29 +1,71 @@
 "use strict";
 
-function showFirstMessege(text) {
-    console.log(text);
+// function showFirstMessege(text) {
+//     console.log(text);
     
-}
-showFirstMessege("Hi mate");
+// }
+// showFirstMessege("Hi mate");
 
-function calc (a, b) {
-    return (a + b);
-}
+// function calc (a, b) {
+//     return (a + b);
+// }
 
-console.log(calc(2, 3));
+// console.log(calc(2, 3));
 
 
-function ret() {
-    let num = 50;
-    return num;
-}
-const anothNum = ret();
-console.log(anothNum);
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+// const anothNum = ret();
 
-const logger = function() {
-    console.log('Hit Jupiter');
-};
+// console.log(anothNum);
+// console.log(ret());
 
-logger();
+// const logger = function() {
+//     console.log('Hit Jupiter');
+// };
 
-const calc1 = (a, b) => (a + b);
+// logger();
+
+// const calc1 = (a, b) => (a + b);
+
+ const options = {
+     name: 'test',
+     width: 1024,
+     height: 1024,
+     colors: {
+         border: 'black',
+         bg: 'red'
+     },
+     makeTest: function() {             // написання власного методу типу .назва 
+         console.log('Testing');
+     }
+ };
+
+ const {border, bg} = options.colors; //деструктуризація об'єкту | ми витягрули об'єкт з іншого об'єкту в якості змінної
+
+
+// console.log(options.name);
+// delete options.name;
+// console.log(options.name);
+
+//  перебір + підрахунок усіх пар в об'єкті
+
+// let counter = 0;
+// for (let key in options) { 
+//     if (typeof(options[key]) === 'object') {
+
+//         for (let i in options[key]) {
+//             console.log(`Option: ${i} and have ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Option: ${key} and have ${options[key]}`);
+//         counter++;
+//     }
+// } 
+// console.log(counter);
+
+console.log(Object.keys(options).length); //  перебір + підрахунок усіх пар в об'єкті 2й простіший варіант
+
